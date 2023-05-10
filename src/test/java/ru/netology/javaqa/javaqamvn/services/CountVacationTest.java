df2c93338a4a82a4691c87097df05cdd0db6977a
Artem <s-art7@bk.ru>
2023-05-10 15:54:57 +0300

    ChangeToParametrizedTest

--- src/test/java/ru/netology/javaqa/javaqamvn/services/CountVacationTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/CountVacationTest.java
@@ -1,27 +1,15 @@
 package ru.netology.javaqa.javaqamvn.services;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CountVacationTest {
 
-    @Test
-    public void testFirstCase() {
-        int threshold = 20_000;
-        int money = 0;
-        int income = 10_000;
-        int expense = 3_000;
-
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/incomingValues.csv")
+    public void testFirstCase(int threshold, int money, int income, int expense) {
         CountVacation service = new CountVacation();
         int monthOfVacation = service.calculate(threshold, money, income, expense);
     }
 
-    @Test
-    public void testSecondCase() {
-        int threshold = 150_000;
-        int money = 0;
-        int income = 100_000;
-        int expense = 60_000;
-        CountVacation service = new CountVacation();
-        int monthOfVacation = service.calculate(threshold, money, income, expense);
-    }
 }
